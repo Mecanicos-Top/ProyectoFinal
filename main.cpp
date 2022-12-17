2af82a3e189a7dbe54ba958b7d3a1d325b1819a7
africa <africa.pariente.fora@opendeusto.es>
2022-12-17 17:39:59 +0100

    1712

--- main.cpp
+++ main.cpp
@@ -11,16 +11,16 @@
 DigitalOut led1(LED1);
 Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
 
- float masa=4.2; 
-// float Vout ; 
- //float G=16.0 ; 
- //int16_t datoV =65536;
+ float masa;
+ float G=16.0 ; 
+ int datoV=60000;
+ //AnalogIn datoV(A3);
  char masa_pantalla[10];
 
 
 I2C i2c(I2C_SDA, I2C_SCL);
 
-
+float calcularMasa(int16_t datoV );
 
  
 int main()
@@ -32,6 +32,7 @@ int main()
         rgbLCD.locate(0,0);
         rgbLCD.print("masa = ");
         rgbLCD.locate(0,1);
+        masa=calcularMasa(datoV);
         sprintf(masa_pantalla, "%f" ,masa);
         rgbLCD.print(masa_pantalla);
         rgbLCD.locate(15,1);
@@ -42,9 +43,11 @@ int main()
 }
 //
 //     Esta función calcula el valor en masa en base a la tensión de salida del acondicionamiento de señal
-int calcularmasa(int16_t datoV )
-{
-    Vout = (datoV / 65536.0) * (5000.0) ; // así se obtiene el dato en mV
+float calcularMasa(int16_t datoV ){
+    float masa;
+    float Vout ; 
+    Vout = (datoV / 4096.0) * (5000.0) ; // así se obtiene el dato en mV
     masa = (Vout) / (5.0 * G) ;  // así se pasa el dato a kg
     return masa;  
 }
+   //
