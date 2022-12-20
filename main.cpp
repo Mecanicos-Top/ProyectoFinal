c115e32c782518b8698bdc8918a41b630456d426
africaapariente <africa.pariente.fora@opendeusto.es>
2022-12-20 17:16:44 +0100

    2012

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 
 #include <stdio.h>
 #include "mbed.h"
 #include "Grove_LCD_RGB_Backlight.h"
- 
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <math.h>
+
+#define WAIT_TIME_MS 500
 
 
-
- 
-#define WAIT_TIME_MS 500 
 DigitalOut led1(LED1);
 Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
 
@@ -29,20 +19,22 @@ Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
     float tension;
 
 I2C i2c(I2C_SDA, I2C_SCL);
-void ajusteLimites(){
-   rgbLCD.print("Buenas tardes abonados");
-   wait_us(1000000);
-   rgbLCD.print("Quiten la carga de la bascula");
-   minimo=tomaDeDatos();
-   rgbLCD.print("Coloquen la carga de 100 g");
-   maximo=tomaDeDatos();
-}
+
 
 float tomaDeDatos(){
     float vector[1000];
     int contador;
     float promedio=0.0;
-    wait_us(3000000);
+    float prueba1=datoV.read_voltage();
+    wait_us(7000000);
+    float prueba2=datoV.read_voltage();
+    if(fabsprueba1-prueba2){
+        rgbLCD.locate(0,0);
+        rgbLCD.print("¿Estais ahi?");
+        rgbLCD.locate(0,1);
+        rgbLCD.print("");
+        wait_us(5000000);
+    }
     for(int i=1;i<=1001;i++){
         vector[i]=datoV.read_voltage();
         contador++;
@@ -64,21 +56,30 @@ int main()
     datoV.set_reference_voltage(3.3);
     while (true)
     {   
-        rgbLCD.print("Buenas tardes abonados");
+        rgbLCD.setRGB(0xff, 0xff, 0xff);//set the color
+        rgbLCD.print("Buenas tardes ");
+        rgbLCD.locate(0,1);
+        rgbLCD.print("abonados");
         wait_us(1000000);
-        rgbLCD.print("Quiten la carga de la bascula");
+        rgbLCD.locate(0,0);
+        rgbLCD.print("Quiten la carga");
+        rgbLCD.locate(0,1);
+        rgbLCD.print("de la bascula");
         minimo=tomaDeDatos();
-        rgbLCD.print("Coloquen la carga de 100 g");
+        rgbLCD.locate(0,0);
+        rgbLCD.print("Coloquen la");
+        rgbLCD.locate(0,1);
+        rgbLCD.print("carga de 100 g");
         maximo=tomaDeDatos();
-        ajusteLimites();
-        rgbLCD.print("Coloquen la carga que pretenden pesar");
+        rgbLCD.locate(0,0);
+        rgbLCD.print("Coloquen la carga");
+        rgbLCD.locate(0,1);
+        rgbLCD.print("que pretenden pesar");
         tension=tomaDeDatos();
-        calcularMasa(tension);
-        rgbLCD.setRGB(0xff, 0xff, 0xff);    //set the color 
+        masa=calcularMasa(tension);
         rgbLCD.locate(0,0);
         rgbLCD.print("masa = ");
         rgbLCD.locate(0,1);
-        masa=(datoV.read_voltage()-0.36)/3.3*100;
         sprintf(masa_pantalla, "%f" ,masa);
         rgbLCD.print(masa_pantalla);
         rgbLCD.locate(15,1);
