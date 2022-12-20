dca7efc4427a375aa450157f6ed2ced6505a3565
africaapariente <africa.pariente.fora@opendeusto.es>
2022-12-20 16:48:36 +0100

    2012

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
  */
 
 
-
-
-
 #include <stdio.h>
 #include "mbed.h"
 #include "Grove_LCD_RGB_Backlight.h"
@@ -17,23 +14,16 @@
  */
 
 
-#include <stdio.h>
-#include "mbed.h"
-#include "Grove_LCD_RGB_Backlight.h"
+
  
 #define WAIT_TIME_MS 500 
 DigitalOut led1(LED1);
 Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
 
  float masa;
- float G=16.0 ;
- AnalogIn lectura(A0);
- AnalogOut alimentacion(A3);
- AnalogOut alimentacionneg(A2);
- int datoV;
  AnalogIn datoV(A3);
  char masa_pantalla[10];
-
+ float Vout;
 
 I2C i2c(I2C_SDA, I2C_SCL);
 
@@ -42,15 +32,14 @@ float calcularMasa(int16_t datoV );
  
 int main()
 {
-    
+    datoV.set_reference_voltage(3.3);
     while (true)
     { 
         rgbLCD.setRGB(0xff, 0xff, 0xff);                 //set the color 
         rgbLCD.locate(0,0);
         rgbLCD.print("masa = ");
         rgbLCD.locate(0,1);
-        datoV=lectura.read();
-        masa=calcularMasa(datoV);
+        masa=(datoV.read_voltage()-0.36)/3.3*100;
         sprintf(masa_pantalla, "%f" ,masa);
         rgbLCD.print(masa_pantalla);
         rgbLCD.locate(15,1);
@@ -63,11 +52,10 @@ int main()
 
 //
 //     Esta función calcula el valor en masa en base a la tensión de salida del acondicionamiento de señal
-float calcularMasa(int16_t datoV ){
+float calcularMasa(float datoV ){
     float masa;
     float Vout ; 
-    Vout = (datoV / 4096.0) * (1000.0) ; // así se obtiene el dato en mV
-    masa = (Vout) / (5.0*G) ;  // así se pasa el dato a kg
-    return masa;  
+    Vout = (datoV / 4096.0) * (1000.0) ; // así se obtiene el dato en mV  // así se pasa el dato a kg
+    return Vout;  
 }
    ////
