cef3f5fe3eac23b7afe8cdf98ef0b9c0eb2f1923
africaapariente <africa.pariente.fora@opendeusto.es>
2022-12-20 16:48:56 +0100

    2012

--- main.cpp
+++ main.cpp
@@ -20,21 +20,60 @@
 DigitalOut led1(LED1);
 Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
 
- float masa;
- AnalogIn datoV(A3);
- char masa_pantalla[10];
- float Vout;
+    float masa;
+    AnalogIn datoV(A3);
+    char masa_pantalla[10];
+    float Vout;
+    float minimo;
+    float maximo;
+    float tension;
 
 I2C i2c(I2C_SDA, I2C_SCL);
+void ajusteLimites(){
+   rgbLCD.print("Buenas tardes abonados");
+   wait_us(1000000);
+   rgbLCD.print("Quiten la carga de la bascula");
+   minimo=tomaDeDatos();
+   rgbLCD.print("Coloquen la carga de 100 g");
+   maximo=tomaDeDatos();
+}
 
-float calcularMasa(int16_t datoV );
-//
+float tomaDeDatos(){
+    float vector[1000];
+    int contador;
+    float promedio=0.0;
+    wait_us(3000000);
+    for(int i=1;i<=1001;i++){
+        vector[i]=datoV.read_voltage();
+        contador++;
+        wait_us(1000);
+        promedio=promedio+vector[i];
+    }
+    promedio=promedio/contador;
+    return promedio;
+}
+
+// Esta función calcula el valor en masa en base a la tensión de salida del acondicionamiento de señal
+float calcularMasa(float datoV){
+    masa=(Vout-minimo)*100/(maximo-minimo);
+    return masa;
+}
  
 int main()
 {
     datoV.set_reference_voltage(3.3);
     while (true)
-    { 
+    {   
+        rgbLCD.print("Buenas tardes abonados");
+        wait_us(1000000);
+        rgbLCD.print("Quiten la carga de la bascula");
+        minimo=tomaDeDatos();
+        rgbLCD.print("Coloquen la carga de 100 g");
+        maximo=tomaDeDatos();
+        ajusteLimites();
+        rgbLCD.print("Coloquen la carga que pretenden pesar");
+        tension=tomaDeDatos();
+        calcularMasa(tension);
         rgbLCD.setRGB(0xff, 0xff, 0xff);                 //set the color 
         rgbLCD.locate(0,0);
         rgbLCD.print("masa = ");
@@ -50,12 +89,4 @@ int main()
 }
 
 
-//
-//     Esta función calcula el valor en masa en base a la tensión de salida del acondicionamiento de señal
-float calcularMasa(float datoV ){
-    float masa;
-    float Vout ; 
-    Vout = (datoV / 4096.0) * (1000.0) ; // así se obtiene el dato en mV  // así se pasa el dato a kg
-    return Vout;  
-}
-   ////
+
